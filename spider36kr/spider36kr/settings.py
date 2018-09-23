efef89886e487ec7c71ffffcd316eb3497ffc5fd
pollord <39593752+pollord@users.noreply.github.com>
2018-09-23 15:12:08 +0800

    Update settings.py

--- spider36kr/spider36kr/settings.py
+++ spider36kr/spider36kr/settings.py
@@ -1,70 +1,30 @@
 # -*- coding: utf-8 -*-
-
-# Scrapy settings for spider36kr project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://doc.scrapy.org/en/latest/topics/settings.html
-#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 import random
+from datetime import datetime
 
 BOT_NAME = 'spider36kr'
-
 SPIDER_MODULES = ['spider36kr.spiders']
 NEWSPIDER_MODULE = 'spider36kr.spiders'
 
 MONGO_URI = 'localhost'
 MONGO_DB = '36kr'
 
-
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_PASSWORD = ''
 REDIS_DB = 2
 
-
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = 3306
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'mysql'
-MYSQL_DATABASE = '36kr'
-
-from datetime import datetime
 today = datetime.now()
 log_file_path = 'log/scrapy_{}_{}_{}.log'.format(today.year, today.month, today.day)
-
 # LOG_LEVEL = 'DEBUG'
 # LOG_FILE = log_file_path
 
-
 PROXY_URL = 'http://localhost:5555/random'
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'spider36kr (+http://www.yourdomain.com)'
 
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
-# CONCURRENT_REQUESTS = 1
-
-# Configure a delay for requests for the same website (default: 0)
-# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
 DOWNLOAD_DELAY = random.randint(1, 3)
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
     'Accept': '*/*',
     'Accept-Encoding': 'gzip, deflate, br',
@@ -77,52 +37,13 @@ DEFAULT_REQUEST_HEADERS = {
     # 'X-Tingyun-Id': 'Dio1ZtdC5G4;r=256032302',
 }
 
-# Enable or disable spider middlewares
-# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'spider36kr.middlewares.Spider36KrSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    # 'spider36kr.middlewares.Spider36KrDownloaderMiddleware': 543,
     'spider36kr.middlewares.ProxyMiddleware': 554,
 }
 
-# Enable or disable extensions
-# See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'spider36kr.pipelines.Spider36KrPipeline': 543,
     'spider36kr.pipelines.MongoPipeline': 545,
-    # 'spider36kr.pipelines.MysqlPipeline': 546,
     'spider36kr.pipelines.RedisPipeline': 544,
 
 }
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
