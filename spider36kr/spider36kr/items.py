5706a0713ce81d4abaf6cb488bc52ad3fc1a560a
pollord <39593752+pollord@users.noreply.github.com>
2018-09-23 15:05:50 +0800

    Update items.py

--- spider36kr/spider36kr/items.py
+++ spider36kr/spider36kr/items.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
@@ -24,23 +18,3 @@ class Spider36KrItem(scrapy.Item):
 
     published_time = scrapy.Field()
     crawled_time = scrapy.Field()
-
-
-# class ArticleItem(scrapy.Item):
-#     article_id = scrapy.Field()
-#     article_url = scrapy.Field()
-#     article_title = scrapy.Field()
-#     article_summary = scrapy.Field()
-#     article_content = scrapy.Field()
-#
-#     article_tags = scrapy.Field()
-#     column_name = scrapy.Field()
-#
-#     crawled_time = scrapy.Field()
-#
-#
-# class WriterItem(scrapy.Item):
-#     writer_name = scrapy.Field()
-#     writer_role = scrapy.Field()
-#     published_time = scrapy.Field()
-#
